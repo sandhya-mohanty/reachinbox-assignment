4e8cab58939a354071b3fd77ddbcb2a8f4479431
Sandhyarani Mohanty <155060146+sandhya-mohanty@users.noreply.github.com>
2024-07-23 14:34:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import Login from './pages/Login'
 import OneBox from './pages/OneBox'
 
  const App = () => {
   return (
     <BrowserRouter>
-    {/* <Header/> */}
     <Routes>
-     
       <Route path='/login' element={<Login/>}/>
       <Route path='/' element={<OneBox/>}/>
-      
     </Routes>
-    {/* <Footer/> */}
     </BrowserRouter>
   )
 }
